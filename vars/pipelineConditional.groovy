94933fa1d41e664572fcb125a988976a75efac87
Josiah Webb <josiah.webb@ezcater.com>
2017-12-15 15:34:38 -0500

    added some debuggingwq!

--- vars/pipelineConditional.groovy
+++ vars/pipelineConditional.groovy
@@ -6,14 +6,19 @@ def call(String filename) {
     def entries = changeLogSets[i].items
 
     for (int j = 0; j < entries.length; j++) {
-      def entry = entries[j]
+      //def entry = entries[j]
       //echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
-      def files = new ArrayList(entry.affectedFiles)
+      //def files = new ArrayList(entry.affectedFiles)
+      def files = new ArrayList(entries[j].affectedFiles)
 
       for (int k = 0; k < files.size(); k++) {
         def file = files[k]
 
-        // return true if filename is in changeset
+        // FOR DEBUGGING:
+        echo(file.path)
+        echo(filename)
+
+        // return true if $filename changed
         if (file.path.indexOf(filename) >= 0) {
           return true
         }
