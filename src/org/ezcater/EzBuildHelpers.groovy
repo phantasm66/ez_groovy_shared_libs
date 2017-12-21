bd229fd7a6836b75366918d753dd7048373189b8
Josiah Webb <josiah.webb@ezcater.com>
2017-12-21 11:47:15 -0500

    zeroing in on finalizing this?

--- src/org/ezcater/EzBuildHelpers.groovy
+++ src/org/ezcater/EzBuildHelpers.groovy
@@ -5,25 +5,19 @@ class EzBuildHelpers implements Serializable {
     def steps
     EzBuildHelpers(steps) {this.steps = steps}
 
+    /* */
+    /* NOTE: when this is ready, we need to change all occurences of phantasm66 to ezcater in this file */
+    /* */
+
     def commitId() {
-        def gitCommitIdFile = 'GIT_COMMIT'
-
-        /* check if we've done this already */
-        if (!gitCommitIdFile.exists()) {
-            steps.sh "git rev-parse HEAD > ${gitCommitIdFile}"
-        }
-
-        def gitCommitId = gitCommitIdFile.text.take(10)
-        return gitCommitId
+        def id = "git rev-parse HEAD".execute().text.trim().take(10)
+        return id
     }
 
-    def specificRepoFilesChanged() {
+    def specificRepoFilesChanged(String gitCommitId) {
         def results = false
-        def gitChangeSetFile = 'CHANGE_SET'
         def localFiles = ['Dockerfile', 'Jenkinsfile', 'docker-entrypoint.sh', 'docker-compose.test.yml']
-
-        steps.sh "git diff-tree --no-commit-id --name-only -r ${this.commitId()} > ${gitChangeSetFile}"
-        def changeSet = gitChangeSetFile.text.tokenize()
+        def changeSet = "git diff-tree --no-commit-id --name-only -r ${gitCommitId}".execute().text.tokenize()
 
         new File('tests').eachFileRecurse(FILES) { foundFile ->
             if (foundFile.name.endsWith('.rb')) {
@@ -41,14 +35,14 @@ class EzBuildHelpers implements Serializable {
     }
 
     def buildDockerImage(String appName, String dockerHubUser, String dockerHubPass) {
-        if (this.specificRepoFilesChanged()) {
-            /* NOTE: will need to change docker hub account from phantasm66 to ezcater eventually */
+        String gitCommitId = this.commitId()
 
-            steps.echo "Building docker image and tagging it: phantasm66/${appName}:${this.commitId()}"
-            steps.sh "/usr/bin/docker build -t phantasm66/${appName}:${this.commitId()} ."
+        if (this.specificRepoFilesChanged(gitCommitId)) {
+            steps.echo "Building docker image and tagging it: phantasm66/${appName}:${gitCommitId}"
+            steps.sh "/usr/bin/docker build -t phantasm66/${appName}:${gitCommitId} ."
 
             /* set $IMAGE_TAG env var so we use this brand new image */
-            steps.sh "export IMAGE_TAG=phantasm66/${appName}:${this.commitId()}"
+            steps.sh "export IMAGE_TAG=phantasm66/${appName}:${gitCommitId}"
             steps.sh "/usr/bin/docker-compose -f docker-compose.tests.yml config"
 
             steps.echo "Launching app locally from docker-compose.test.yml"
@@ -62,13 +56,13 @@ class EzBuildHelpers implements Serializable {
 
             steps.echo "Authenticating w/ docker registry and pushing new image"
             steps.sh "/usr/bin/docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-            steps.sh "/usr/bin/docker push phantasm66/${appName}:${this.commitId()}"
+            steps.sh "/usr/bin/docker push phantasm66/${appName}:${gitCommitId}"
         } else {
             steps.echo "No build related files have changed... skipping image build stage"
         }
     }
 
-    def executeDeploy(String namespace) {
+    def executeDeploy(String kubeNamespace) {
         /* prepare the kube deploy YAMLs from the ezdeploy.yml file by shelling out to ezdeploy.rb */
         /* deploy kube rendered YAMLs to kube (including correct namespace for env (prd or staging)) - also using ezdeploy.rb */
     }
